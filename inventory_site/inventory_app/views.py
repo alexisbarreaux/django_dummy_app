592fa4513ac739a509c70b018ae161b5d6054129
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-11-06 13:52:51 +0100

    Starting to make views
    - Trying to remind myself of how to make html without react

--- inventory_site/inventory_app/views.py
+++ inventory_site/inventory_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.contrib.auth.views import LoginView
+
+from .models import Employee, Store, Product
 
 # Create your views here.
+class EmployeeLogin(LoginView):
+    template_name = "inventory_app/login.html"
+
+
+class IndexView(generic.ListView):
+    template_name = "inventory_app/index.html"
+    context_object_name = "products_from_store"
+    store = 1
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Product.objects.filter(current_store=self.store).order_by(
+            "shortest_expiry_date"
+        )[:20]
