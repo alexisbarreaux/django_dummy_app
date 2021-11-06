ef3c5b952a94a8df55dd66ef485855367bbcaef6
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-11-06 11:28:46 +0100

    Created product first model

--- inventory_site/inventory_app/admin.py
+++ inventory_site/inventory_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Constant data", {"fields": ["GTIN"]}),
+        (
+            "Store dependent date",
+            {"fields": ["current_store", "product_name", "shortest_expiry_date"]},
+        ),
+    ]
+    # To change display in the main page of the products objects instead
+    # of just the __str__ result
+    list_display = ("GTIN", "current_store", "expires_today")
+    # Adds a filter tab
+    list_filter = ["shortest_expiry_date"]
+    # clear
+    search_fields = ["current_store", "GTIN"]
+
+
+admin.site.register(Product, ProductAdmin)
--- inventory_site/inventory_app/models.py
+++ inventory_site/inventory_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from datetime import date
+from django.contrib import admin
+from typing import Optional
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model storing data about one product type.
+
+    Attributes:
+        current_store (str) : shop in which the product is displayed.
+        product_name (str): name of the product in the shop it is in.
+        GTIN (str): unique identifier of the product.
+        shortest_expiry_date (Optional[date]): Date when the first displayed product
+            of this type (this GTIN) will expire in the shope. Defaults to current date.
+
+    """
+
+    def current_date():
+        return date.today()
+
+    current_store: str = models.CharField("Shop", max_length=200)
+    product_name: str = models.CharField("Name", max_length=200)
+    GTIN: str = models.CharField(max_length=14)
+    shortest_expiry_date: Optional[date] = models.DateField(
+        "Products expire at", default=current_date
+    )
+    #    expiry_dates: List[date]
+    # )
+    last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+    @admin.display(
+        boolean=True,
+        ordering="shortest_expiry_date",  # Order by date on click
+        description="Expires today",  # To change name
+    )
+    def expires_today(self):
+        now = date.today()
+        return now == self.shortest_expiry_date
