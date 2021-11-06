8d8d18da0785c51d2c5456a47265899a63d221f6
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-11-06 11:59:30 +0100

    Adding store and employee models and migrate

--- inventory_site/inventory_app/admin.py
+++ inventory_site/inventory_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Employee, Store
+
+
+class StoreAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ["lastname"]
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -7,16 +15,18 @@ class ProductAdmin(admin.ModelAdmin):
         ("Constant data", {"fields": ["GTIN"]}),
         (
             "Store dependent date",
-            {"fields": ["current_store", "product_name", "shortest_expiry_date"]},
+            {"fields": ["current_store", "name", "shortest_expiry_date"]},
         ),
     ]
     # To change display in the main page of the products objects instead
     # of just the __str__ result
-    list_display = ("GTIN", "current_store", "expires_today")
+    list_display = ("GTIN", "current_store", "shortest_expiry_date", "expires_today")
     # Adds a filter tab
     list_filter = ["shortest_expiry_date"]
     # clear
     search_fields = ["current_store", "GTIN"]
 
 
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Product, ProductAdmin)
--- inventory_site/inventory_app/models.py
+++ inventory_site/inventory_app/models.py
@@ -1,7 +1,38 @@
 from django.db import models
-from datetime import date
+from datetime import date, datetime
 from django.contrib import admin
 from typing import Optional
+
+
+class Store(models.Model):
+    """Class storing data about a store.
+
+    Attributes:
+        name (str): Name of the store.
+    """
+
+    # Mostly empty
+    name: str = models.CharField("Store name", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    """Class to store data about an employee
+
+    Attributes:
+        firstname (str): First name of the employee.
+        lastname (str): Last name of the employee.
+        current_store (str): Name of the store in which the employee works.
+    """
+
+    current_store: str = models.ForeignKey(Store, on_delete=models.CASCADE)
+    firstname: str = models.CharField("First name", max_length=40)
+    lastname: str = models.CharField("Last name", max_length=40, unique=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
 
 
 class Product(models.Model):
@@ -9,28 +40,28 @@ class Product(models.Model):
 
     Attributes:
         current_store (str) : shop in which the product is displayed.
-        product_name (str): name of the product in the shop it is in.
+        name (str): name of the product in the shop it is in.
         GTIN (str): unique identifier of the product.
         shortest_expiry_date (Optional[date]): Date when the first displayed product
             of this type (this GTIN) will expire in the shope. Defaults to current date.
-
+        last_modified  (datetime): Datetime to know when this object was last modified.
     """
 
     def current_date():
         return date.today()
 
-    current_store: str = models.CharField("Shop", max_length=200)
-    product_name: str = models.CharField("Name", max_length=200)
+    current_store: str = models.ForeignKey(Store, on_delete=models.CASCADE)
+    name: str = models.CharField("Name", max_length=100)
     GTIN: str = models.CharField(max_length=14)
     shortest_expiry_date: Optional[date] = models.DateField(
         "Products expire at", default=current_date
     )
+    last_modified: datetime = models.DateTimeField(auto_now=True)
     #    expiry_dates: List[date]
     # )
-    last_modified = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.product_name
+        return self.name
 
     @admin.display(
         boolean=True,
