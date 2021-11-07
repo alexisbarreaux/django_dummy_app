85d28593a56dad495a6ffe848d66d6d7712e749c
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2021-11-07 12:30:02 +0100

    Starting to add some style

--- inventory_site/inventory_site/settings.py
+++ inventory_site/inventory_site/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
